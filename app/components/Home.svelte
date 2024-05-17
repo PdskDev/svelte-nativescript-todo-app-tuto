56c0c0a70911eef16105c2a8059828ce1c352b36
Nadet LAMBI-BIDZIMOU <nadetdev@gmail.com>
2024-05-17 20:05:02 +0200

    initi commit

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,15 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
-<script lang="ts">
-    let message: string = "Blank Svelte Native App"
+<script>
+    const { tabView, tabViewItem }=require("@nativescript/core");
 </script>
+    <page>
+      <actionBar title="My Tasks" />
+      <tabView>
+        <tabViewItem title="To Do">
+            <label textWrap="true">Active tasks</label>
+        </tabViewItem>
+        <tabViewItem title="Completed">
+            <label textWrap="true">Completed tasks</label>
+        </tabViewItem>
+      </tabView>
+    </page>
 
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
